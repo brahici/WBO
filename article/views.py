bd12ecccd720f1460b9f4247e9cd0d89d34e51d8
Brice V <brahici@gmail.com>
2012-06-19 16:18:36 +0200

    Added a mixin for filtered articles lists

--- article/views.py
+++ article/views.py
@@ -5,13 +5,25 @@ from django.views.generic import ListView
 from django.views.generic.dates import YearArchiveView, MonthArchiveView
 from .models import Article, Category
 
+class FilteredArticleListMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(FilteredArticleListMixin, self).get_context_data(
+                **kwargs)
+        context['articles_filtered'] = True
+        return context
 
-class CategoryArticleList(ListView):
+
+class CategoryArticleList(FilteredArticleListMixin, ListView):
     context_object_name = 'articles'
     template_name = 'article/index.html'
     def get_queryset(self):
-        category = get_object_or_404(Category, slug__iexact=self.kwargs['slug'])
-        return Article.published.filter(category=category)
+        self.category = get_object_or_404(Category, slug__iexact=self.kwargs['slug'])
+        return Article.published.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryArticleList, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
 
 
 class CallableQuerySetMixin(object):
@@ -20,17 +32,23 @@ class CallableQuerySetMixin(object):
         return self.queryset()
 
 
-class ArticleArchiveMixin(CallableQuerySetMixin):
+class ArticleArchiveMixin(CallableQuerySetMixin, FilteredArticleListMixin):
     context_object_name = 'articles'
     template_name = 'article/index.html'
     date_field = 'published_date'
     queryset = Article.published.all
 
+    def get_context_data(self, **kwargs):
+        context = super(ArticleArchiveMixin, self).get_context_data(**kwargs)
+        context['year'] = self.kwargs['year']
+        return context
 
-class ArticleYearArchive(ArticleArchiveMixin, YearArchiveView):
+class ArticleYearArchive(ArticleArchiveMixin, FilteredArticleListMixin,
+        YearArchiveView):
     make_object_list = True
 
 
-class ArticleMonthArchive(ArticleArchiveMixin, MonthArchiveView):
+class ArticleMonthArchive(ArticleArchiveMixin, FilteredArticleListMixin,
+        MonthArchiveView):
     month_format = '%m'
 
